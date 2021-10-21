ad75b92ef590f59d4a4cb14a8fe4e67cfb667ba0
opcc <opcc22059@gmail.com>
2021-10-21 21:17:44 +0800

    script to swap use y to buy x

---
+++ scripts/swapY2X.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+const settings = require("../.settings.js");
+
+async function main() {
+    [signer, trader] = await ethers.getSigners();
+    console.log("trader: ", trader.address);
+    const Swap = await ethers.getContractFactory("Swap");
+    swap = Swap.attach(settings.swapAddr);
+
+    const tokenContract = await ethers.getContractFactory("Token");
+
+    const usdc = tokenContract.attach(settings.usdc);
+    await usdc.connect(trader).approve(settings.swapAddr, '10000000000000000000000');
+
+    const usdt = tokenContract.attach(settings.usdt);
+    await usdt.connect(trader).approve(settings.swapAddr, '10000000000000000000000');
+    console.log(await usdt.allowance(trader.address, settings.swapAddr));
+
+    tx = await swap.connect(trader).swapY2X(
+        usdc.address, usdt.address, 3000, "50000000000000", 6001
+    );
+    
+    console.log("swap tx: ", tx);
+}
+
+main().then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
