bd817186f9724a026f6684bfb8f52ad223cb64a6
opcc <opcc22059@gmail.com>
2021-11-01 22:41:41 +0800

    add multicall interface

--- contracts/base/base.sol
+++ contracts/base/base.sol
@@ -20,6 +20,23 @@ abstract contract Base {
     address public immutable factory;
     address public immutable WETH9;
 
+    function multicall(bytes[] calldata data) external payable returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+
+            if (!success) {
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577
+                if (result.length < 68) revert();
+                assembly {
+                    result := add(result, 0x04)
+                }
+                revert(abi.decode(result, (string)));
+            }
+
+            results[i] = result;
+        }
+    }
     /// @notice Transfers tokens from the targeted address to the given destination
     /// @notice Errors with 'STF' if transfer fails
     /// @param token The contract address of the token to be transferred
