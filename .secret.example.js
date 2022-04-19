e13fd8b9cca50eeb5b385ec121c567efdafbe43c
opcc <opcc22059@gmail.com>
2022-04-19 19:09:38 +0800

    update

--- .secret.example.js
+++ .secret.example.js
@@ -1,15 +1,3 @@
 module.exports = {
     sk: '${PRIMARY KEY of ACCOUNT1}',
-    weth: '${ADDRESS of WETH}'.toLowerCase(),
-    izumiswapfactory: '${ADDRESS of IzumiswapFactory}',
-    nflmAddr: '${ADDRESS of Nonfungible Liquidity Manager}',
-    nflomAddr: '${ADDRESS of Limit Order Manager'},
-    swapAddr: '${ADDRESS of SWAP}',
-    quoterAddr: '${ADDRESS of QUOTER}',
-    BIT: '${ADDRESS of TOKEN BIT}',
-    USDC: '${ADDRESS of TOKEN USDC}',
-    // usdc: '0xa5ecCB4d91d2c306a6b1f2F9820986feA80aAc8d'.toLowerCase(),
-    // usdt: '0xDa12894F72D5e302138Da6d356aAF8eD1df4d0C7'.toLowerCase(),
-    // pool_usdc_usdt: '0xd4Ab1c773F530Aa900B1c521Be6c25078E3003a5',
-    // pool_usdc_weth: '0xB42B2EEcE144bDc88765BA49bec1eaCB15dbE1f7',
 }
