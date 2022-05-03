e2527fb081e71f24272325173df26c6da7f9203c
opcc <opcc22059@gmail.com>
2022-05-03 16:50:52 +0800

    update

--- scripts/nftLiquidityManager/deployNFLM.js
+++ scripts/nftLiquidityManager/deployNFLM.js
@@ -10,12 +10,13 @@ HARDHAT_NETWORK='izumiTest' node scripts/nftLiquidityManager/deployNFLM.js
 */
 
 const net = process.env.HARDHAT_NETWORK
+const v = process.argv
+const weth = v[2]
 
 async function main() {
     // deploy nft
     const LiquidityManager = await ethers.getContractFactory("LiquidityManager");
     const iZiSwapFactory = deployed[net].iZiSwapFactory;
-    const weth = deployed[net].WETH9
     nflm = await LiquidityManager.deploy(iZiSwapFactory, weth);
     console.log("LiquidityManager: ", nflm.address);
     await nflm.deployed();
