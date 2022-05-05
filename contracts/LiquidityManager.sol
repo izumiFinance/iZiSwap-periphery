7fcdb99d15c0a685e27c921e157be6985c5add4e
McGrady00H <281130306@qq.com>
2022-05-05 12:39:53 +0800

    update

--- contracts/LiquidityManager.sol
+++ contracts/LiquidityManager.sol
@@ -29,7 +29,7 @@ contract LiquidityManager is Base, ERC721Enumerable, IiZiSwapMintCallback {
     }
 
     // max-poolId in poolIds, poolId starts from 1
-    uint128 maxPoolId = 1;
+    uint128 private maxPoolId = 1;
 
     // infomation of liquidity provided by miner
     struct Liquidity {
