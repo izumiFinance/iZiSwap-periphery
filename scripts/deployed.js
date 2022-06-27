b4a1ac40530eb0674a31038e43c5701bffc3c00e
Mac <MacRen000@gmail.com>
2022-06-27 15:50:14 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -1,10 +1,13 @@
 const contracts = {
     bscTest: {
-        iZiSwapFactory: '0xe1fE5791aa0BdC0137393Cf19AfaDf1ddac3E249',
+        iZiSwapFactory: '0xfc601E5D885d0545ee1B20EA1711D1F1c0eA7d0A',
         WETH9: '0xa9754f0D9055d14EB0D2d196E4C51d8B2Ee6f4d3',
+	    liquidityManager: '0x6bEae78975e561fDF27AaC6f09F714E69191DcfD',
+	    BIT: '0xac360dc0F8EF1C94Ab4034220E0A01567acbFdE4',
+	    USDT: '0x6AECfe44225A50895e9EC7ca46377B9397D1Bb5b',
     },
     bsc: {
-        iZiSwapFactory: '0x1502d025BfA624469892289D45C0352997251728',
+        iZiSwapFactory: '0xd7de110Bd452AAB96608ac3750c3730A17993DE0',
         WBNB: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     }
 }
