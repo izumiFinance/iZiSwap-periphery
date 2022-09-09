c6ca6a72c7799ad723f6987a7f937fda4619cd6b
opcc <opcc22059@gmail.com>
2022-09-09 19:18:42 +0800

    update

---
+++ scripts/quoter/deployQuoterWithLim.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+const deployed = require('../deployed.js');
+
+/*
+
+example: 
+
+HARDHAT_NETWORK='izumiTest' node scripts/swap/deploySwap.js
+
+*/
+
+const net = process.env.HARDHAT_NETWORK
+const v = process.argv
+const weth = v[2];
+
+async function main() {
+
+    const factory = deployed[net].iZiSwapFactory;
+    // deploy Quoter
+    const Quoter = await ethers.getContractFactory("QuoterWithLim");
+    const quoter = await Quoter.deploy(factory, weth);
+    await quoter.deployed();
+    console.log("quoter: ", quoter.address);
+}
+
+main().then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
