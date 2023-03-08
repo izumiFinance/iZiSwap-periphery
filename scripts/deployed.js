611ddfa2a91d2ffcc805f36ae0c3417c6fd8af0b
opcc <opcc22059@gmail.com>
2023-03-08 15:03:29 +0800

    update

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -24,10 +24,10 @@ const contracts = {
 	swap: '0xa9754f0D9055d14EB0D2d196E4C51d8B2Ee6f4d3',
     },
     mantleTest: {
-        iZiSwapFactory: '0x64c2F1306b4ED3183E7B345158fd01c19C0d8c5E',
-        wrapChainToken: '0x67A1f4A939b477A6b7c5BF94D97E45dE87E608eF',
-        liquidityManager: '0xC6C7c2edF70A3245ad6051E93809162B9758ce08',
-        swap: '0xa9754f0D9055d14EB0D2d196E4C51d8B2Ee6f4d3',
+        iZiSwapFactory: '0x848a0C239e1C42785f71e16B1d5c48D503fd0797',
+        wrapChainToken: '0x69AC69b272f96F5f17DDD9da3832ad9Dc86D1d8A',
+        liquidityManager: '0xaDd9336AB4a795a66E4E1820f5d347c4294dd76a',
+        swap: '0x3A507CB35fcDe6445EF6e3D44D836f81d24374B8',
     },
     bscTest: {
         iZiSwapFactory: '0x7fc0574eAe768B109EF38BC32665e6421c52Ee9d',
