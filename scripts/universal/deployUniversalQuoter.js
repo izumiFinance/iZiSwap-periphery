8069d8919c6dfc524b06a33af87a68e0dd681650
opcc <opcc22059@gmail.com>
2024-05-22 17:44:05 +0800

    add script to deploy universal router/contracts

---
+++ scripts/universal/deployUniversalQuoter.js
@@ -0,0 +1,36 @@
+const { ethers } = require("hardhat");
+const deployed = require('../deployed.js');
+
+
+const net = process.env.HARDHAT_NETWORK
+const v = process.argv
+
+const para = {
+    classicFactory: v[2],
+    swapRouter: v[3],
+}
+
+async function main() {
+
+    const iZiSwapFactory = deployed[net].iZiSwapFactory;
+    console.log("Paramters: ");
+    console.log('iZiSwapFactory: ', iZiSwapFactory)
+    for ( var i in para) { console.log("    " + i + ": " + para[i]); }
+  
+    console.log('=====================');
+    // deploy swap
+    const Quoter = await ethers.getContractFactory("UniversalQuoter");
+    const quoter = await Quoter.deploy(
+        iZiSwapFactory,
+        para.classicFactory,
+        para.swapRouter,
+    );
+    await quoter.deployed();
+    console.log("quoter: ", quoter.address);
+}
+
+main().then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
---
+++ scripts/universal/deployUniversalRouter.js
@@ -0,0 +1,40 @@
+const { ethers } = require("hardhat");
+const deployed = require('../deployed.js');
+
+
+const net = process.env.HARDHAT_NETWORK
+const v = process.argv
+
+const para = {
+    classicFactory: v[2],
+    weth: v[3],
+    charger: v[4],
+    outFee: v[5],
+}
+
+async function main() {
+
+    const iZiSwapFactory = deployed[net].iZiSwapFactory;
+    console.log("Paramters: ");
+    console.log('iZiSwapFactory: ', iZiSwapFactory)
+    for ( var i in para) { console.log("    " + i + ": " + para[i]); }
+  
+    console.log('=====================');
+    // deploy swap
+    const SwapRouter = await ethers.getContractFactory("UniversalSwapRouter");
+    const router = await SwapRouter.deploy(
+        iZiSwapFactory,
+        para.classicFactory,
+        para.weth,
+        para.charger,
+        para.outFee
+    );
+    await router.deployed();
+    console.log("router: ", router.address);
+}
+
+main().then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
