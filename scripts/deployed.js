1aa4007bca0e0704cd0c45389e8d1f4e15075a03
opcc <opcc22059@gmail.com>
2024-07-01 15:49:40 +0800

    rm xlayer config

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -219,9 +219,6 @@ const contracts = {
     morphTest: {
         iZiSwapFactory: '0x64c2F1306b4ED3183E7B345158fd01c19C0d8c5E',
     },
-    XLayer: {
-        iZiSwapFactory: '0x8c7d3063579BdB0b90997e18A770eaE32E1eBb08',
-    },
     BOB: {
         iZiSwapFactory: '0x8c7d3063579BdB0b90997e18A770eaE32E1eBb08',
     },
